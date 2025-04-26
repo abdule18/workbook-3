a06105749a82f0fecf439838a767c909b5e72fb5
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-26 12:45:12 -0400

    Pushing the third update of the payroll calculator

--- PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -4,34 +4,67 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-//    Employee employee = new Employee(1, "Abdule", 40, 25);
+
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
         String fileName = "employees.csv";
 
-//        FileInputStream fileInputStream = null;
+
         try {
-//            FileInputStream  fileInputStream = new FileInputStream(fileName);
 
-            BufferedReader bufReader = new BufferedReader(new FileReader(fileName));
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter(fileName));
 
-            String line;
-            while ((line = bufReader.readLine()) != null) {
-                System.out.println(line);
+            bufWriter.write("ID|Name|Gross Pay");
+            bufWriter.newLine();
+
+            System.out.println("How many employees do you want to add?");
+            int numEmpl = scanner.nextInt();
+            scanner.nextLine();
+
+            for (int i =0; i < numEmpl; i++){
+                System.out.print("Enter ID: ");
+                String id = scanner.nextLine();
+
+                System.out.print("Enter employer name: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Enter the employee Gross Pay: ");
+                String grossPay = scanner.nextLine();
+
+                bufWriter.write(id + "|" + name + "|" + grossPay);
+                bufWriter.newLine();
             }
 
-            String[] parts = line.split("\\|");
+//            System.out.println("Enter the name of the file employee file to process: ");
+//            String file = scanner.nextLine();
+//            System.out.println();
 
-            int id = Integer.parseInt(parts[0]);
-            String name = parts[1];
-            float hoursWorked = Float.parseFloat(parts[2]);
-            double payRate = Double.parseDouble(parts[3]);
+//            if(file.equalsIgnoreCase(fileName)){
+//
+//            }
+//
+//            int line;
+//
+//            for (line = 1; line <= bufWriter.length; line++){
+//                bufWriter.write(bufWriter[line]);
+//            }
 
-            Employee employee = new Employee(id, name, hoursWorked, payRate);
 
-            System.out.printf("ID: %d | Name: %s | Pay: $%.2f%n",
-                    employee.getEmployeeId(), employee.getName(), employee.getHoursWorked(), employee.getPayRate());
+//            String[] parts = line.split("\\|");
+//
+//            int id = Integer.parseInt(parts[0]);
+//            String name = parts[1];
+//            float hoursWorked = Float.parseFloat(parts[2]);
+//            double payRate = Double.parseDouble(parts[3]);
+//
+//            Employee employee = new Employee(id, name, hoursWorked, payRate);
+//
+//            System.out.printf("ID: %d | Name: %s | Pay: $%.2f%n",
+//                    employee.getEmployeeId(), employee.getName(), employee.getHoursWorked(), employee.getPayRate());
 
-            bufReader.close();
+            bufWriter.close();
+            System.out.println("This is the outcome: " + fileName);
         } catch (IOException e) {
             System.out.println("There is an Error!");;
         }
